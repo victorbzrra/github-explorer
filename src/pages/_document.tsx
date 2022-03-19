66a0e09a3e1680dc925c11d197ff445a091a763b
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-03-18 21:14:56 -0300

    :sparkles: styles: Adding favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,7 +8,7 @@ export default class MyDocument extends Document{
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,600;1,400&family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
         
-          {/* <link rel="shortcut icon" href="/favicon.png" type="image/png" /> */}
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <Main />
