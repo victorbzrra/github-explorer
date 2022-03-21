034e5df450bbbfaefff8e2f9c80e19a674cfccbf
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-03-21 00:05:47 -0300

    :construction: feat: Github auth

--- src/components/SignOutButton/index.tsx
+++ src/components/SignOutButton/index.tsx
@@ -1,15 +1,21 @@
+import { signOut, useSession } from "next-auth/react";
 import { AiOutlineClose, AiOutlineGithub } from "react-icons/ai";
 
 import styles from "./styles.module.scss";
 
 export function SignOutButton() {
+  const { data: session } = useSession();
+  
   return (
     <button
       type="button"
       className={styles.signOutButton}
+      onClick={() => {
+        signOut({callbackUrl: "/"});
+      }}
     >
       <AiOutlineGithub />
-      Victor Bezerra
+      {session?.user?.name}
       <AiOutlineClose />
     </button>
   )
