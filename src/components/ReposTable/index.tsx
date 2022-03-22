71ea50f87b64c660ad2e49e65ffa25df236d5f0a
Victor Bezerra <victorbzrra@alu.ufc.br>
2022-03-22 15:06:02 -0300

    :construction: feat: Adding scroll table repositories

--- src/components/ReposTable/index.tsx
+++ src/components/ReposTable/index.tsx
@@ -5,35 +5,37 @@ import styles from './styles.module.scss';
 
 export function ReposTable({option, publicRepos, starredRepos}: ReposDataProps) {
   return (
-    <table className={styles.tableContainer}>
-      <thead className={styles.tableHead}>
-        <tr>
-          <th>Nome</th>
-          <th>Quant. Estrelas</th>
-          <th>Data de Criação</th>
-        </tr>
-      </thead>
-      <tbody className={styles.tableBody}>
-        {option
-          ? publicRepos?.map((repo) => (
-              <tr key={repo.name}>
-                <td>
-                  <a href={repo.html_url}>{repo.name}</a>
-                </td>
-                <td>{repo.stargazers_count}</td>
-                <td>{moment(repo.created_at).format("DD/MM/YYYY")}</td>
-              </tr>
-            ))
-          : starredRepos?.map((repo) => (
-              <tr key={repo.name}>
-                <td>
-                  <a href={repo.html_url}>{repo.name}</a>
-                </td>
-                <td>{repo.stargazers_count}</td>
-                <td>{moment(repo.created_at).format("DD/MM/YYYY")}</td>
-              </tr>
-            ))}
-      </tbody>
-    </table>
+    <div className={styles.scrollContainer}>
+      <table className={styles.tableContainer}>
+        <thead className={styles.tableHead}>
+          <tr>
+            <th>Nome</th>
+            <th>Quant. Estrelas</th>
+            <th>Data de Criação</th>
+          </tr>
+        </thead>
+        <tbody className={styles.tableBody}>
+          {option
+            ? publicRepos?.map((repo) => (
+                <tr key={repo.name}>
+                  <td>
+                    <a href={repo.html_url}>{repo.name}</a>
+                  </td>
+                  <td>{repo.stargazers_count}</td>
+                  <td>{moment(repo.created_at).format("DD/MM/YYYY")}</td>
+                </tr>
+              ))
+            : starredRepos?.map((repo) => (
+                <tr key={repo.name}>
+                  <td>
+                    <a href={repo.html_url}>{repo.name}</a>
+                  </td>
+                  <td>{repo.stargazers_count}</td>
+                  <td>{moment(repo.created_at).format("DD/MM/YYYY")}</td>
+                </tr>
+              ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
